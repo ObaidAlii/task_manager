0b8e8018e081d55efafc54db894a83c4947728df
Obaid Ali <obaidali30@gmail.com>
2025-09-12 03:05:47 +0530

    fixed database connection string

--- backend/db_config/postgresConnection.js
+++ backend/db_config/postgresConnection.js
@@ -1,12 +1,16 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// const pgPool = new Pool({
+//   user: process.env.DBUSER,
+//   host: process.env.DBHOST,
+//   password: process.env.DBPASSWORD,
+//   database: process.env.DBNAME,
+//   port: process.env.DBPORT,
+// });
 const pgPool = new Pool({
-  user: process.env.DBUSER,
-  host: process.env.DBHOST,
-  password: process.env.DBPASSWORD,
-  database: process.env.DBNAME,
-  port: process.env.DBPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pgPool;
